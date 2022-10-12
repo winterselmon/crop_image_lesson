c21d0db0e80403836637896d700e2b9941bc49e1
winterselmon <bestlovedream23@gmail.com>
2022-10-12 11:07:08 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,154 @@
+import 'dart:math';
+
+import 'package:custom_image_crop/custom_image_crop.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/result_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late CustomImageCropController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CustomImageCropController();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: CustomImageCrop(
+                cropController: controller,
+                // image: const AssetImage('assets/test.png'), // Any Imageprovider will work, try with a NetworkImage for example...
+                image: const NetworkImage(
+                    'https://upload.wikimedia.org/wikipedia/en/7/7d/Lenna_%28test_image%29.png'),
+                shape: CustomCropShape.Square,
+              ),
+            ),
+            Row(
+              children: [
+                IconButton(
+                    icon: const Icon(Icons.refresh),
+                    onPressed: controller.reset),
+                IconButton(
+                    icon: const Icon(Icons.zoom_in),
+                    onPressed: () =>
+                        controller.addTransition(CropImageData(scale: 1.33))),
+                IconButton(
+                    icon: const Icon(Icons.zoom_out),
+                    onPressed: () =>
+                        controller.addTransition(CropImageData(scale: 0.75))),
+                IconButton(
+                    icon: const Icon(Icons.rotate_left),
+                    onPressed: () => controller
+                        .addTransition(CropImageData(angle: -pi / 4))),
+                IconButton(
+                    icon: const Icon(Icons.rotate_right),
+                    onPressed: () =>
+                        controller.addTransition(CropImageData(angle: pi / 4))),
+                IconButton(
+                  icon: const Icon(Icons.crop),
+                  onPressed: () async {
+                    final image = await controller.onCropImage();
+                    debugPrint(image.toString());
+                    if (image != null) {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              ResultScreen(image: image)));
+                    }
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: MediaQuery.of(context).padding.bottom),
+          ],
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/result_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class ResultScreen extends StatelessWidget {
+  final MemoryImage image;
+
+  const ResultScreen({
+    required this.image,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint(image.bytes.toString());
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Result'),
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            Image(
+              image: image,
+            ),
+            ElevatedButton(
+              child: const Text('Back'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
